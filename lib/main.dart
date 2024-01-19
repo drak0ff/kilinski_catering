765066ae9cb4dfbd566f7b4fe9b0c032f2be78b2
Daniel Bortsov <wretched666bunny@gmail.com>
2024-01-19 22:39:53 +0100

    firestore innitwq

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:happy_catering/app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:happy_catering/firebase_options.dart';
 import 'package:user_repository/user_repository.dart';
 
 import 'simple_bloc_observer.dart';
