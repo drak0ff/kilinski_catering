5d332cf3148b04b575b99d8cb6f6300e9448d34e
Daniel Bortsov <wretched666bunny@gmail.com>
2024-03-01 09:45:39 +0100

    Changed error data displaying. Now when client for some reason, does not have diets data, HappyCateringDietDataCard.dart will notify about it. Added field controll data and default value to bottom text in HappyCateringDietDataCard

--- lib/screens/home/components/diets_carousel/HappyCateringDietDataCard.dart
+++ lib/screens/home/components/diets_carousel/HappyCateringDietDataCard.dart
@@ -3,12 +3,14 @@ import 'package:happy_catering/screens/order_details/menu_details.dart';
 
 class HappyCateringDietDataCard extends StatelessWidget {
   final String pictureUrl;
-  final String dietName;
+  final String bottomText;
+  final bool hasData;
 
   const HappyCateringDietDataCard({
     Key? key,
-    required this.pictureUrl,
-    required this.dietName,
+    required this.hasData,
+    this.pictureUrl = "",
+    this.bottomText = "No data available right now",
   }) : super(key: key);
 
   @override
@@ -16,79 +18,119 @@ class HappyCateringDietDataCard extends StatelessWidget {
     final colorScheme = Theme.of(context).colorScheme;
 
     return Container(
-      margin: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12),
-        border: Border.all(color: colorScheme.primary), // Add border color
-      ),
-      child: Container(
-        padding: const EdgeInsets.all(16),
+        margin: const EdgeInsets.all(16),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(12),
-          color: colorScheme.secondary,
+          border: Border.all(color: colorScheme.primary), // Add border color
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 120,
-              width: 200,
-              child: Container(
-                width: 150,
-                height: 100,
+        child: hasData
+            ? Container(
+                padding: const EdgeInsets.all(16),
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(12),
                   color: colorScheme.secondary,
                 ),
-                child: Image.asset(
-                  pictureUrl, // Replace with your image asset
-                  width: 100,
-                  height: 100,
-                  fit: BoxFit.cover,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: 120,
+                      width: 200,
+                      child: Container(
+                        width: 150,
+                        height: 100,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: colorScheme.secondary,
+                        ),
+                        child: Image.asset(
+                          pictureUrl, // Replace with your image asset
+                          width: 100,
+                          height: 100,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Container(
+                      child: Text(
+                        bottomText,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 24,
+                          color: Colors.black,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 2,
+                        softWrap: true,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    OutlinedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const MenuDetails()));
+                      },
+                      style: OutlinedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 32.0, vertical: 8.0),
+                        side: BorderSide(
+                            color: colorScheme.primary), // Outline color
+                      ),
+                      child: Text(
+                        'Order >',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 15,
+                          color: colorScheme.tertiary,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ),
-            const SizedBox(height: 8),
-            Container(
-              child: Text(
-                dietName,
-                style: const TextStyle(
-                  fontWeight: FontWeight.w400,
-                  fontSize: 24,
-                  color: Colors.black,
+              )
+            : Container(
+                padding: const EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: colorScheme.secondary,
                 ),
-                overflow: TextOverflow.ellipsis,
-                maxLines: 2,
-                softWrap: true,
-                textAlign: TextAlign.center,
-              ),
-            ),
-            const SizedBox(height: 8),
-            OutlinedButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const MenuDetails()));
-              },git
-              style: OutlinedButton.styleFrom(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 32.0, vertical: 8.0),
-                side: BorderSide(color: colorScheme.primary), // Outline color
-              ),
-              child: Text(
-                'Order >',
-                style: TextStyle(
-                  fontWeight: FontWeight.w600,
-                  fontSize: 15,
-                  color: colorScheme.tertiary,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: 110,
+                        width: 110,
+                        child: Container(
+                          width: 150,
+                          height: 100,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            color: colorScheme.secondary,
+                          ),
+                          child: Image.asset(
+                            'assets/img/cancel.png',
+                            width: 100,
+                            height: 100,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        bottomText,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 14,
+                          color: Colors.black,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ])));
   }
 }
